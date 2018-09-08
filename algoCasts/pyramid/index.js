6901d7493dd47b71690a4f3391f23e1937a429f0
Nabin Purja <nabinpurja93@gmail.com>
2018-09-08 11:27:34 +0100

    pyramid - iterative and recursive solutions

---
+++ algoCasts/pyramid/index.js
@@ -0,0 +1,56 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left *and* right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+
+function pyramid(n, row = 0, level = '') {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    level += '#';
+  } else {
+    level += ' ';
+  }
+
+  pyramid(n, row, level);
+}
+
+module.exports = pyramid;
+
+// function pyramid(n) {
+//   // calculate the midpoint index
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
